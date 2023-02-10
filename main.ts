8c315fc6707c7fad4633fb298dd5d8a6abdab48b
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-10 15:20:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
 })
 radio.setGroup(200)
 basic.forever(function () {
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -1 && KSB045.getJoystickValue(KSB045.valueType.X) <= 1) {
+    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -10 && KSB045.getJoystickValue(KSB045.valueType.X) < 10) {
         radio.sendNumber(4)
         basic.showLeds(`
             . . . . #
@@ -23,20 +23,18 @@ basic.forever(function () {
             `)
         basic.pause(100)
         basic.clearScreen()
-    }
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) < 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -1 && KSB045.getJoystickValue(KSB045.valueType.X) <= 1) {
-        radio.sendNumber(6)
+    } else if (KSB045.getJoystickValue(KSB045.valueType.X) > 0 && KSB045.getJoystickValue(KSB045.valueType.Y) >= -10 && KSB045.getJoystickValue(KSB045.valueType.Y) < 10) {
+        radio.sendNumber(8)
         basic.showLeds(`
-            # . . . .
-            . # . . .
+            . . . . .
+            . . . . .
             . . # . .
-            . # . . .
-            # . . . .
+            . # . # .
+            # . . . #
             `)
         basic.pause(100)
         basic.clearScreen()
-    }
-    if (KSB045.getJoystickValue(KSB045.valueType.X) < 0 && KSB045.getJoystickValue(KSB045.valueType.Y) >= -1 && KSB045.getJoystickValue(KSB045.valueType.Y) <= 1) {
+    } else if (KSB045.getJoystickValue(KSB045.valueType.X) != 0 && KSB045.getJoystickValue(KSB045.valueType.Y) >= -10 && KSB045.getJoystickValue(KSB045.valueType.Y) < 10) {
         radio.sendNumber(2)
         basic.showLeds(`
             # . . . #
@@ -47,15 +45,14 @@ basic.forever(function () {
             `)
         basic.pause(100)
         basic.clearScreen()
-    }
-    if (KSB045.getJoystickValue(KSB045.valueType.X) > 0 && KSB045.getJoystickValue(KSB045.valueType.Y) >= -1 && KSB045.getJoystickValue(KSB045.valueType.Y) <= 1) {
-        radio.sendNumber(8)
+    } else {
+        radio.sendNumber(6)
         basic.showLeds(`
-            . . . . .
-            . . . . .
+            # . . . .
+            . # . . .
             . . # . .
-            . # . # .
-            # . . . #
+            . # . . .
+            # . . . .
             `)
         basic.pause(100)
         basic.clearScreen()
