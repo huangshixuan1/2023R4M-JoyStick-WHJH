93e896413584fe39f11d166b9485b527cf8e5267
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-10 14:56:21 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
 })
 radio.setGroup(200)
 basic.forever(function () {
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) < 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -10 && KSB045.getJoystickValue(KSB045.valueType.X) <= 10) {
+    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -10 && KSB045.getJoystickValue(KSB045.valueType.X) <= 10) {
         radio.sendNumber(4)
         basic.showLeds(`
             . . . . #
@@ -24,7 +24,7 @@ basic.forever(function () {
         basic.pause(100)
         basic.clearScreen()
     }
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -10 && KSB045.getJoystickValue(KSB045.valueType.X) <= 10) {
+    if (KSB045.getJoystickValue(KSB045.valueType.Y) < 0 && KSB045.getJoystickValue(KSB045.valueType.X) >= -10 && KSB045.getJoystickValue(KSB045.valueType.X) <= 10) {
         radio.sendNumber(6)
         basic.showLeds(`
             # . . . .
