124bf86bf346036286ee4a43d6c186bd98b18b4a
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-22 12:32:00 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
         . . . # .
         . # # . .
         `)
-    basic.pause(100)
+    basic.pause(50)
     basic.clearScreen()
 })
 KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
@@ -19,7 +19,7 @@ KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
         . . # . .
         . . # . .
         `)
-    basic.pause(100)
+    basic.pause(50)
     basic.clearScreen()
 })
 radio.setGroup(171)
