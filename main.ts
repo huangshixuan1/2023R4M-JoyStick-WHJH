5db332c1a0e28b2bf69516443e642faace4fc878
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-07-20 10:09:03 +0800

    Delete Lights

--- main.ts
+++ main.ts
@@ -2,48 +2,12 @@ radio.setGroup(98)
 basic.forever(function () {
     if (KSB045.getJoystickValue(KSB045.valueType.X) < 500 && KSB045.getJoystickValue(KSB045.valueType.Y) >= 400 && KSB045.getJoystickValue(KSB045.valueType.Y) < 600) {
         radio.sendNumber(4)
-        basic.pause(50)
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # . . . .
-            . # . . .
-            . . # . .
-            `)
-        basic.clearScreen()
     } else if (KSB045.getJoystickValue(KSB045.valueType.Y) < 500 && KSB045.getJoystickValue(KSB045.valueType.X) >= 400 && KSB045.getJoystickValue(KSB045.valueType.Y) < 600) {
         radio.sendNumber(8)
-        basic.pause(50)
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . . . #
-            . . . . .
-            . . . . .
-            `)
-        basic.clearScreen()
     } else if (KSB045.getJoystickValue(KSB045.valueType.X) > 540 && KSB045.getJoystickValue(KSB045.valueType.Y) >= 400 && KSB045.getJoystickValue(KSB045.valueType.Y) < 600) {
         radio.sendNumber(6)
-        basic.pause(50)
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            . . . . #
-            . . . # .
-            . . # . .
-            `)
-        basic.clearScreen()
     } else if (KSB045.getJoystickValue(KSB045.valueType.Y) > 520 && KSB045.getJoystickValue(KSB045.valueType.X) >= 400 && KSB045.getJoystickValue(KSB045.valueType.X) < 600) {
         radio.sendNumber(2)
-        basic.pause(50)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-        basic.clearScreen()
     } else if (KSB045.getBtnValue(KSB045.btnName.E)) {
         radio.sendNumber(1)
     } else if (KSB045.getBtnValue(KSB045.btnName.C)) {
